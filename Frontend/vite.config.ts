c01832ebf8fdb14e32bac721162323f16b0cf9f2
sammy6378 <samuel202mwangi@gmail.com>
2024-07-24 09:23:45 +0300

    update vite config file

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+ 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Ensure this is 'dist'
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          // Add more manual chunks as necessary
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust this limit as necessary
+  },
+});
